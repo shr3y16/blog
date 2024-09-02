d60250faa662e4df79a7083f10537f49408aa553
shr3y16 <shreyaskalrao2001@gmail.com>
2024-09-02 19:52:20 +0530

    add db port

--- database.js
+++ database.js
@@ -8,6 +8,7 @@ const db = createConnection({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT
 });
 
 export default db;
