3ba1a7a12befed32f77b2d802dd5f3dcde09f46c
shr3y16 <shreyaskalrao2001@gmail.com>
2024-09-13 20:00:01 +0530

    update database settings

--- database.js
+++ database.js
@@ -1,14 +1,17 @@
-import { createConnection } from "mysql2";
+import { createPool } from "mysql2";
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
 
-const db = createConnection({
+const db = createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.DATABASE_PORT
+    port: process.env.DATABASE_PORT,
+    waitForConnections: true,  
+    connectionLimit: 10,     
+    queueLimit: 0              
 });
 
 export default db;
