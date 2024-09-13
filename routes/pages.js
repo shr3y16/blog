e7e84f5b29abc1e02b4ada86ee1db478694115ad
shr3y16 <shreyaskalrao2001@gmail.com>
2024-09-13 20:26:20 +0530

    display email of the blogger as subtitle

--- routes/pages.js
+++ routes/pages.js
@@ -5,14 +5,19 @@ const pageRouter = Router();
 
 pageRouter.get("/", loggedIn, (req, res) => {
 
-    db.query('SELECT * FROM blogs', (err, blogs) => {
-        if (err) throw err;
-        if (req.user) {
-            res.render('index', { status: "loggedIn", user: req.user, blogs: blogs });
-        } else {
-            res.render('index', { status: "no", blogs: blogs });
-        }
-    });
+    db.query(`
+        SELECT blogs.*, users.email 
+        FROM blogs 
+        JOIN users ON blogs.user_id = users.user_id`, 
+        (err, blogs) => {
+            if (err) throw err;
+            if (req.user) {
+                res.render('index', { status: "loggedIn", user: req.user, blogs: blogs });
+            } else {
+                res.render('index', { status: "no", blogs: blogs });
+            }
+        });
+    
 })
 
 pageRouter.get("/register", (req, res) => {
